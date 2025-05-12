efd9c3cc97b744fdab98a590af9531e9f869f5a8
Carlos Fernandez <dominguezalacid@gmail.com>
2025-05-12 15:44:31 +0200

    actualizando curriculum

---
+++ 16 - Extras/Print-PDF/script.js
@@ -0,0 +1,23 @@
+document.getElementById('download').addEventListener('click', () => {
+    const cvElement = document.getElementById('page1');
+
+    // Import jsPDF
+    const { jsPDF } = window.jspdf;
+
+    // Crear un nuevo jsPDF en formato A4 y orientación vertical
+    const pdf = new jsPDF('p', 'mm', 'a4');
+
+    // Función para añadir el contenido HTML al PDF
+    pdf.html(cvElement, {
+        callback: function (pdf) {
+            // Guardar el PDF
+            pdf.save("CV-Carlos-Fernandez.pdf");
+        },
+        margin: [10, 10, 10, 10], // Márgenes
+        autoPaging: 'text',
+        x: 0,
+        y: 0,
+        width: 190, // Ajustar el ancho según sea necesario
+        windowWidth: 800 // Ancho de la ventana
+    });
+});
